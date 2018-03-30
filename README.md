fda7c4d4037c8bcd4e1d40351096e6ff2e7bc171
Ambroise Maupate <ambroise@rezo-zero.com>
2018-03-30 20:43:11 +0200

    Add logic to PostContainerTrait

--- README.md
+++ README.md
@@ -26,3 +26,36 @@ public static function setupDependencyInjection(Container $container)
     });
 }
 ```
+
+## PostContainerTrait
+
+`PostContainerTrait` will implement your `indexAction` by handling all request data
+to provide your posts, filters and available tags to build your template.
+
+IndexAction will assign:
+
+- `posts`: found `NodesSources` array according to your criteria
+- `currentTag`: tag or `null`
+- `filters`: pagination information array
+- `tags`: available filtering `Tag` array
+
+### Usage
+
+All you need to do is creating your node-source `Controller` in your theme and 
+implements `ConfigurableController` and use `PostContainerControllerTrait`.
+You will be able to override any methods to configure your blog listing.
+
+
+```php
+<?php
+namespace Themes\MyTheme\Controllers;
+
+use Themes\AbstractBlogTheme\Controllers\ConfigurableController;
+use Themes\AbstractBlogTheme\Controllers\PostContainerControllerTrait;
+use Themes\MyTheme\MyThemeThemeApp;
+
+class BlogPostContainerController extends MyThemeThemeApp implements ConfigurableController
+{
+    use PostContainerControllerTrait;
+}
+```
--- src/Controllers/PostContainerControllerTrait.php
+++ src/Controllers/PostContainerControllerTrait.php
@@ -2,11 +2,14 @@
 
 namespace Themes\AbstractBlogTheme\Controllers;
 
+use Doctrine\ORM\QueryBuilder;
 use RZ\Roadiz\Core\Entities\Node;
+use RZ\Roadiz\Core\Entities\Tag;
 use RZ\Roadiz\Core\Entities\Translation;
 use RZ\Roadiz\Core\ListManagers\EntityListManager;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Exception\ResourceNotFoundException;
 
 trait PostContainerControllerTrait
 {
@@ -23,31 +26,141 @@ trait PostContainerControllerTrait
     ) {
         $this->prepareThemeAssignation($node, $translation);
 
+        if ($this->get('blog_theme.post_entity') === false) {
+            throw new \RuntimeException('blog_theme.post_entity must be configured with your own BlogPost node-type class');
+        }
+
         /** @var EntityListManager $elm */
         $elm = $this->createEntityListManager(
             $this->get('blog_theme.post_entity'),
-            $this->getDefaultCriteria($translation),
+            $this->getDefaultCriteria($translation, $request->query->get('tag'), $request->query->get('archive')),
             $this->getDefaultOrder()
         );
         $elm->setItemPerPage($this->getItemsPerPage());
         $elm->handle();
 
-        $this->assignation['posts'] = $elm->getEntities();
+        $posts = $elm->getEntities();
+
+        if (count($posts) === 0) {
+            throw $this->createNotFoundException('No post found for given criteria.');
+        }
+
+        $this->assignation['posts'] = $posts;
+        $this->assignation['currentTag'] = $this->getTag($request->query->get('tag'));
         $this->assignation['filters'] = $elm->getAssignation();
+        $this->assignation['tags'] = $this->getAvailableTags();
 
         return $this->render($this->getTemplate(), $this->assignation);
     }
 
     /**
+     * @return string
+     */
+    protected function getPublicationField()
+    {
+        return 'publishedAt';
+    }
+
+    /**
+     * @param string $tagName
+     *
+     * @return Tag|null
+     */
+    protected function getTag($tagName = '')
+    {
+        if ($tagName != '') {
+            return $this->get('em')->getRepository(Tag::class)->findOneByTagName($tagName);
+        }
+
+        return null;
+    }
+
+    /**
      * @return array
      */
-    protected function getDefaultCriteria(Translation $translation)
+    protected function getDefaultCriteria(Translation $translation, $tagName = '', $archive = '')
     {
-        return [
+        $criteria = [
             'node.visible' => true,
             'translation' => $translation,
-            'publishedAt' => ['<=', new \DateTime()],
+            $this->getPublicationField() => ['<=', new \DateTime()],
         ];
+
+        if ($tagName != '') {
+            $tag = $this->getTag($tagName);
+            if (null === $tag) {
+                throw $this->createNotFoundException('Tag does not exist.');
+            }
+            $criteria['tags'] = $tag;
+        }
+
+        if ($archive != '') {
+            if (preg_match('#[0-9]{4}\-[0-9]{2}#', $archive) > 0) {
+                $startDate = new \DateTime($archive . '-01 00:00:00');
+                $endDate = clone $startDate;
+                $endDate->add(new \DateInterval('P1M'));
+
+                $criteria[$this->getPublicationField()] = ['BETWEEN', $startDate, $endDate];
+            } elseif (preg_match('#[0-9]{4}#', $archive) > 0) {
+                $startDate = new \DateTime($archive . '-01-01 00:00:00');
+                $endDate = clone $startDate;
+                $endDate->add(new \DateInterval('P1Y'));
+
+                $criteria[$this->getPublicationField()] = ['BETWEEN', $startDate, $endDate];
+            } else {
+                throw $this->createNotFoundException('Archive filter is malformed.');
+            }
+        }
+
+        return $criteria;
+    }
+
+    /**
+     * @param Tag $parentTag Parent tag
+     * @return array
+     */
+    protected function getAvailableTags(Tag $parentTag = null)
+    {
+        /** @var QueryBuilder $qb */
+        $qb = $this->get('em')
+            ->getRepository(Tag::class)
+            ->createQueryBuilder('t');
+
+        /** @var QueryBuilder $subQb */
+        $subQb = $this->get('em')
+                      ->getRepository($this->get('blog_theme.post_entity'))
+                      ->createQueryBuilder('p');
+
+        try {
+            $qb->select('t')
+                ->innerJoin('t.nodes', 'n')
+                ->innerJoin('n.nodeSources', 'ns')
+                ->andWhere($qb->expr()->in('ns.id', $subQb->select('p.id')->getDQL()))
+                ->andWhere($qb->expr()->eq('t.visible', true));
+
+            if (null !== $parentTag) {
+                $parentTagId = $parentTag->getId();
+                $qb->innerJoin('t.parent', 'pt')
+                    ->andWhere('pt.id = :parent')
+                    ->setParameter('parent', $parentTagId);
+            }
+
+            $this->alterTagQueryOrderBy($qb);
+
+            return $qb->getQuery()->getResult();
+        } catch (NoResultException $e) {
+            return [];
+        }
+    }
+
+    /**
+     * @param QueryBuilder $queryBuilder
+     *
+     * @return QueryBuilder
+     */
+    protected function alterTagQueryOrderBy(QueryBuilder $queryBuilder)
+    {
+        return $queryBuilder->addOrderBy('t.position', 'ASC');
     }
 
     /**
@@ -56,7 +169,7 @@ trait PostContainerControllerTrait
     protected function getDefaultOrder()
     {
         return [
-            'publishedAt' => 'DESC'
+            $this->getPublicationField() => 'DESC'
         ];
     }
 
